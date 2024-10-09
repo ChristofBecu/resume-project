e9bbbcb4f56da2a68bc0c88b0faa0983048e6468
Christof Becu <christof.becu@gmail.com>
2024-10-09 15:46:08 +0200

    replace secret with js

---
+++ replaceEnv.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+const envFilePath = path.resolve(__dirname, 'src/environments/environment.prod.ts');
+const gistResumeUrl = process.env.GISTRESUME;
+
+fs.readFile(envFilePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Error reading file: ${err}`);
+    process.exit(1);
+  }
+
+  const result = data.replace(/gistResumeUrl: ''/, `gistResumeUrl: '${gistResumeUrl}'`);
+
+  fs.writeFile(envFilePath, result, 'utf8', (err) => {
+    if (err) {
+      console.error(`Error writing file: ${err}`);
+      process.exit(1);
+    }
+    console.log('Environment variable replaced successfully.');
+  });
+});
