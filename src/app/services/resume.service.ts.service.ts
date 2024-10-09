1a0158dad8b2f49ea900aaff125caadc97d98c08
Christof Becu <christof.becu@gmail.com>
2024-10-09 15:53:53 +0200

    add proxy to prevent cors error

--- src/app/services/resume.service.ts.service.ts
+++ src/app/services/resume.service.ts.service.ts
@@ -1,6 +1,6 @@
 // src/app/services/resume.service.ts
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
+import { catchError, from, Observable, of } from 'rxjs';
 import { environment } from '../../environments/environment.prod';
 
 @Injectable({
@@ -10,10 +10,17 @@ export class ResumeService {
   private gistUrl: string;
 
   constructor() {
-    this.gistUrl = environment.gistResumeUrl || '/assets/resume.json';
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+    this.gistUrl = corsProxy + (environment.gistResumeUrl || '/assets/resume.json');
   }
 
   getResumeData(): Observable<any> {
-    return from(fetch(this.gistUrl).then(response => response.json()));
+    return from(fetch(this.gistUrl).then(response => response.json())).pipe(
+      catchError(error => {
+        console.error('Error fetching resume data:', error);
+        // Return an observable with a user-friendly error message or fallback data
+        return of({ error: 'Failed to fetch resume data' });
+      })
+    );
   }
 }
